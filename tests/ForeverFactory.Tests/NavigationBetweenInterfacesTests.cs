f07737cd38e71e72916ca7a2b19acf5c88b89468
Dyego Maas <dyego.maas@gmail.com>
2021-06-20 11:34:24 -0300

    Refactoring customized factories tests.

--- tests/ForeverFactory.Tests/NavigationBetweenInterfacesTests.cs
+++ tests/ForeverFactory.Tests/NavigationBetweenInterfacesTests.cs
@@ -1,16 +1,19 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using ForeverFactory.Tests.ExampleFactories;
 using FluentAssertions;
+using ForeverFactory.Builders;
 using Xunit;
 
 namespace ForeverFactory.Tests
 {
     public class NavigationBetweenInterfacesTests
     {
-        [Fact]
-        public void PlusOne_should_chain_previous_ManyBuilder_to_a_new_LinkedOneBuilder()
+        [Theory]
+        [MemberData(nameof(FactoryInitializationVariants))]
+        public void PlusOne_should_chain_previous_ManyBuilder_to_a_new_LinkedOneBuilder(ICustomizableFactory<Person> factory)
         {
-            var persons = new PersonFactory()
+            var persons = factory
                 .Many(count: 5).With(x => x.Age = 99)
                 .PlusOne().With(x => x.Age = 100)
                 .Plus(count: 5).With(x => x.Age = 97)
@@ -38,37 +41,12 @@ namespace ForeverFactory.Tests
             var last = persons.Skip(11).First();
             last.Age.Should().Be(101);
         }
-        
-        [Fact]
-        public void PlusOne_should_chain_previous_ManyBuilder_to_a_new_LinkedOneBuilder2()
-        {
-            var persons = MagicFactory.For<Person>()
-                .Many(count: 5).With(x => x.Age = 99)
-                .PlusOne().With(x => x.Age = 100)
-                .Plus(count: 5).With(x => x.Age = 97)
-                .PlusOne().With(x => x.Age = 101)
-                .Build()
-                .ToList();
 
-            persons.Should().HaveCount(12);
-            
-            var firstTen = persons.Take(5);
-            foreach (var person in firstTen)
+        public static IEnumerable<object[]> FactoryInitializationVariants =>
+            new List<object[]>
             {
-                person.Age.Should().Be(99);
-            }
-
-            var sixth = persons[5];
-            sixth.Age.Should().Be(100);
-            
-            var nextFive = persons.Skip(6).Take(5);
-            foreach (var person in nextFive)
-            {
-                person.Age.Should().Be(97);
-            }
-            
-            var last = persons.Skip(11).First();
-            last.Age.Should().Be(101);
-        }
+                new object[] { new PersonFactory() },
+                new object[] { MagicFactory.For<Person>() },
+            };
     }
 }
