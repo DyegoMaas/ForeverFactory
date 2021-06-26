a72d8de7d2322d796e60e5f9775b5d8219ed7017
Dyego Maas <dyego.maas@gmail.com>
2021-06-25 21:46:56 -0300

    Refactoring testes and writing some new.

--- tests/ForeverFactory.Tests/NavigationBetweenInterfacesTests.cs
+++ tests/ForeverFactory.Tests/NavigationBetweenInterfacesTests.cs
@@ -15,60 +15,73 @@ namespace ForeverFactory.Tests
                 new object[] {new PersonFactory()},
                 new object[] {MagicFactory.For<Person>()}
             };
-
+        
         [Theory]
         [MemberData(nameof(FactoryInitializationVariants))]
-        public void Should_allow_navigation_starting_from_many(ISimpleFactory<Person> factory)
+        public void It_should_be_possible_to_chain_another_one_after_many(ISimpleFactory<Person> factory)
         {
             var persons = factory
-                .Many(5).With(x => x.Age = 99)
+                .Many(2).With(x => x.Age = 99)
                 .PlusOne().With(x => x.Age = 100)
-                .Plus(5).With(x => x.Age = 97)
-                .PlusOne().With(x => x.Age = 101)
                 .Build()
                 .ToList();
 
-            persons.Should().HaveCount(12);
+            persons.Should().HaveCount(3);
 
-            var firstTen = persons.Take(5);
-            foreach (var person in firstTen) person.Age.Should().Be(99);
-
-            var sixth = persons[5];
-            sixth.Age.Should().Be(100);
-
-            var nextFive = persons.Skip(6).Take(5);
-            foreach (var person in nextFive) person.Age.Should().Be(97);
-
-            var last = persons.Skip(11).First();
-            last.Age.Should().Be(101);
+            persons[0].Age.Should().Be(99);
+            persons[1].Age.Should().Be(99);
+            persons[2].Age.Should().Be(100);
         }
-
+        
         [Theory]
         [MemberData(nameof(FactoryInitializationVariants))]
-        public void It_should_be_possible_to_explicitly_say_you_are_building_one_instance(ISimpleFactory<Person> factory)
+        public void It_should_be_possible_to_chain_many_more_after_many(ISimpleFactory<Person> factory)
+        {
+            var persons = factory
+                .Many(2).With(x => x.Age = 99)
+                .Plus(2).With(x => x.Age = 100)
+                .Build()
+                .ToList();
+
+            persons.Should().HaveCount(4);
+
+            persons[0].Age.Should().Be(99);
+            persons[1].Age.Should().Be(99);
+            persons[2].Age.Should().Be(100);
+            persons[3].Age.Should().Be(100);
+        }
+        
+        [Theory]
+        [MemberData(nameof(FactoryInitializationVariants))]
+        public void It_should_be_possible_to_chain_another_one_builder(ISimpleFactory<Person> factory)
         {
             var persons = factory
                 .One().With(x => x.Age = 99)
-                .Plus(4).With(x => x.Age = 99)
                 .PlusOne().With(x => x.Age = 100)
-                .Plus(5).With(x => x.Age = 97)
-                .PlusOne().With(x => x.Age = 101)
                 .Build()
                 .ToList();
 
-            persons.Should().HaveCount(12);
+            persons.Should().HaveCount(2);
 
-            var firstTen = persons.Take(5);
-            foreach (var person in firstTen) person.Age.Should().Be(99);
+            persons[0].Age.Should().Be(99);
+            persons[1].Age.Should().Be(100);
+        }
+        
+        [Theory]
+        [MemberData(nameof(FactoryInitializationVariants))]
+        public void It_should_be_possible_to_chain_many_after_one(ISimpleFactory<Person> factory)
+        {
+            var persons = factory
+                .One().With(x => x.Age = 99)
+                .Plus(2).With(x => x.Age = 100)
+                .Build()
+                .ToList();
 
-            var sixth = persons[5];
-            sixth.Age.Should().Be(100);
+            persons.Should().HaveCount(3);
 
-            var nextFive = persons.Skip(6).Take(5);
-            foreach (var person in nextFive) person.Age.Should().Be(97);
-
-            var last = persons.Skip(11).First();
-            last.Age.Should().Be(101);
+            persons[0].Age.Should().Be(99);
+            persons[1].Age.Should().Be(100);
+            persons[2].Age.Should().Be(100);
         }
     }
 }
