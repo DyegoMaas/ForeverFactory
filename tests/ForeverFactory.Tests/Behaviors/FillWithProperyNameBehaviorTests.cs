05445d6773b42e328e3d43eb7667d03ac2585416
Dyego Maas <dyego.maas@gmail.com>
2022-01-25 22:17:56 -0300

    Filling values with property names plus sequential number now works for string properties.
    No support for recursive filling for now.

--- tests/ForeverFactory.Tests/Behaviors/FillWithProperyNameBehaviorTests.cs
+++ tests/ForeverFactory.Tests/Behaviors/FillWithProperyNameBehaviorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using ForeverFactory.Behaviors;
 using ForeverFactory.FluentInterfaces;
@@ -17,13 +18,24 @@ namespace ForeverFactory.Tests.Behaviors
 
         [Theory]
         [MemberData(nameof(PersonFactoriesWithFillPropertiesWithEmptyValuesBehavior))]
-        public void It_should_fill_all_properties_with_empty_values(ISimpleFactory<Customer> factory)
+        public void It_should_fill_all_properties_with_property_names(ISimpleFactory<Customer> factory)
         {
             var customer = factory.Build();
 
             customer.Name.Should().Be("Name1");
             customer.Address.Should().NotBeNull();
             customer.Address.ZipCode.Should().Be("ZipCode1");
+        }
+        
+        [Theory]
+        [MemberData(nameof(PersonFactoriesWithFillPropertiesWithEmptyValuesBehavior))]
+        public void It_should_fill_all_properties_with_property_names_with_sequential_numbers(ISimpleFactory<Customer> factory)
+        {
+            var customer = factory.Many(3).Build().ToArray();
+
+            customer[0].Name.Should().Be("Name1");
+            customer[1].Name.Should().Be("Name2");
+            customer[2].Name.Should().Be("Name3");
         }
 
         public class Customer
