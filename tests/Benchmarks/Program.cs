feb84b993569ed1ec8b38a7b55875c9c0efd8b86
Dyego Maas <dyego.maas@gmail.com>
2022-01-25 21:15:56 -0300

    Added benchmarks.

---
+++ tests/Benchmarks/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using FizzWare.NBuilder;
+using ForeverFactory;
+
+var summary = BenchmarkRunner.Run(typeof(BuildersBenchmark));
+
+[MemoryDiagnoser]
+public class BuildersBenchmark
+{
+    private const string PersonName = "Arnold";
+
+    [Benchmark]
+    public void BuildSingleObjectForeverFactory()
+    {
+        MagicFactory.For<Person>().With(x => x.Name = PersonName).Build();
+    }
+    
+    [Benchmark]
+    public void BuildSingleObjectNBuilder()
+    {
+        Builder<Person>.CreateNew().With(x => x.Name = PersonName).Build();
+    }
+    
+    [Benchmark]
+    public void BuildThousandObjectsForeverFactory()
+    {
+        MagicFactory.For<Person>().Many(1000).With(x => x.Name = PersonName).Build().ToList();
+    }
+    
+    [Benchmark]
+    public void BuildThousandObjectsNBuilder()
+    {
+        Builder<Person>.CreateListOfSize(1000).All().With(x => x.Name = PersonName).Build();
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+}
