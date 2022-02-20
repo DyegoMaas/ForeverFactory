11886819a4ad4d80be610dbe574f61e3a364ecfe
Dyego Alekssander Maas <dyego.maas@gmail.com>
2022-02-20 08:57:59 -0300

    Refactoring tests.

--- tests/ForeverFactory.Tests/Factories/LayeredTransformApplicationTests.cs
+++ tests/ForeverFactory.Tests/Factories/LayeredTransformApplicationTests.cs
@@ -10,7 +10,7 @@ public class LayeredTransformApplicationTests
     [Fact]
     public void should_apply_behavior_first()
     {
-        var instance = new SomeClassFactory(customize: null)
+        var instance = new SomeClassFactoryWithNoOverridenProperty()
             .WithBehavior(new FillWithEmptyValuesBehavior())
             .Build();
 
@@ -20,8 +20,7 @@ public class LayeredTransformApplicationTests
     [Fact]
     public void factory_customization_should_override_behavior_transforms()
     {
-        var instance = new SomeClassFactory(customization => customization
-                .Set(x => x.Name = "John Doe"))
+        var instance = new SomeClassFactoryWithOverridenProperty()
             .WithBehavior(new FillWithEmptyValuesBehavior())
             .Build();
 
@@ -31,8 +30,7 @@ public class LayeredTransformApplicationTests
     [Fact]
     public void scenario_customization_should_override_factory_transforms()
     {
-        var instance = new SomeClassFactory(customization => customization
-                .Set(x => x.Name = "John Doe"))
+        var instance = new SomeClassFactoryWithOverridenProperty()
             .WithBehavior(new FillWithEmptyValuesBehavior())
             .With(x => x.Name = "New Name")
             .Build();
@@ -40,21 +38,21 @@ public class LayeredTransformApplicationTests
         instance.Name.Should().Be("New Name");
     }
     
-    public class SomeClassFactory : MagicFactory<SomeClass>
+    public class SomeClassFactoryWithNoOverridenProperty : MagicFactory<SomeClass>
     {
-        private readonly Action<ICustomizeFactoryOptions<SomeClass>> _customize;
-
-        public SomeClassFactory(Action<ICustomizeFactoryOptions<SomeClass>> customize)
-        {
-            _customize = customize;
-        }
-
         protected override void Customize(ICustomizeFactoryOptions<SomeClass> customization)
         {
-            _customize?.Invoke(customization);
         }
     }
-
+    
+    public class SomeClassFactoryWithOverridenProperty : MagicFactory<SomeClass>
+    {
+        protected override void Customize(ICustomizeFactoryOptions<SomeClass> customization)
+        {
+            customization.Set(x => x.Name = "John Doe");
+        }
+    }
+    
     public class SomeClass
     {
         public string Name { get; set; }
