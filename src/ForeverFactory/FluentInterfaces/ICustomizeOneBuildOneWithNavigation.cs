7cb59637154bbd94fa7543fc0311373c8641d5d1
Dyego Alekssander Maas <dyego.maas@gmail.com>
2022-02-21 20:20:29 -0300

    Updating documentation.

--- src/ForeverFactory/FluentInterfaces/ICustomizeOneBuildOneWithNavigation.cs
+++ src/ForeverFactory/FluentInterfaces/ICustomizeOneBuildOneWithNavigation.cs
@@ -5,7 +5,7 @@ namespace ForeverFactory.FluentInterfaces
     public interface ICustomizeOneBuildOneWithNavigation<out T> : IBuildOne<T>, INavigable<T>
     {
         /// <summary>
-        ///     Defines the default value of a property.
+        ///     Defines the value of a property.
         /// </summary>
         /// <param name="setMember">Sets the value of a Property.
         ///     <example>x => x.Name = "Karen"</example>
