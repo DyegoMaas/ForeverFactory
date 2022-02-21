1c3019370fac640d8f5142a592295fdb2f75eca4
Dyego Alekssander Maas <dyego.maas@gmail.com>
2022-02-21 20:20:29 -0300

    Updating documentation.

--- src/ForeverFactory/FluentInterfaces/ICustomizeManyBuildMany.cs
+++ src/ForeverFactory/FluentInterfaces/ICustomizeManyBuildMany.cs
@@ -9,7 +9,7 @@ namespace ForeverFactory.FluentInterfaces
     public interface ICustomizeManyBuildMany<out T> : IBuildMany<T>, INavigable<T>
     {
         /// <summary>
-        ///     Defines the default value of a property for all instances.
+        ///     Defines the value of a property for all instances.
         /// </summary>
         /// <param name="setMember">Sets the value of a Property.
         ///     <example>x => x.Name = "Karen"</example>
@@ -17,7 +17,7 @@ namespace ForeverFactory.FluentInterfaces
         ICustomizeManyBuildMany<T> With<TValue>(Func<T, TValue> setMember);
 
         /// <summary>
-        ///     Defines the default value of a property for the first N instances.
+        ///     Defines the value of a property for the first N instances.
         /// </summary>
         /// <param name="count">How many instances will have this property set.</param>
         /// <param name="setMember">Sets the value of a Property.
@@ -26,7 +26,7 @@ namespace ForeverFactory.FluentInterfaces
         ICustomizeManyBuildMany<T> WithFirst<TValue>(int count, Func<T, TValue> setMember);
 
         /// <summary>
-        ///     Defines the default value of a property for the last N instances.
+        ///     Defines the value of a property for the last N instances.
         /// </summary>
         /// <param name="count">How many instances will have this property set.</param>
         /// <param name="setMember">Sets the value of a Property.
