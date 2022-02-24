a13cabdf4bd99c4fa24703fc97c7aa9ac6759c6a
Dyego Alekssander Maas <dyego.maas@gmail.com>
2022-02-23 23:13:16 -0300

    Correcting merge.

--- tests/ForeverFactory.Tests/Factories/CallbackTests.cs
+++ tests/ForeverFactory.Tests/Factories/CallbackTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using ForeverFactory.FluentInterfaces;
 using Xunit;
 
 namespace ForeverFactory.Tests.Factories;
@@ -6,23 +9,90 @@ namespace ForeverFactory.Tests.Factories;
 public class CallbackTests
 {
     [Fact]
-    public void Should_expectation()
+    public void should_execute_callbacks_in_order()
     {
-        var person = MagicFactory.For<Person>()
-            .Do(x => x.Email = $"{x.FirstName}.{x.LastName}@gmail.com".ToLower())
-            
-            
-            .With(x => x.FirstName = "Albert")
-            .With(x => x.LastName = "Einstein")
+        string shipNameBefore = null;
+        string shipNameAfter = null;
+        
+        MagicFactory.For<Ship>()
+            .Do(x => shipNameBefore = x.Name)
+            .With(x => x.Name = "Mary")
+            .Do(x => shipNameAfter = x.Name)
             .Build();
 
-        person.Email.Should().Be("albert.einstein@gmail.com");
+        shipNameBefore.Should().BeNull();
+        shipNameAfter.Should().Be("Mary");
+    }
+    
+    [Fact]
+    public void should_execute_callback_through_ICustomizeOneBuildOne()
+    {
+        string shipName = null;
+        
+        Action<Ship> saveShipName = ship => shipName = ship.Name;
+        ICustomizeOneBuildOne<Ship> factory = MagicFactory.For<Ship>()
+            .With(x => x.Name = "Mary");
+        factory
+            .Do(saveShipName)
+            .Build();
+
+        shipName.Should().Be("Mary");
+    }
+    
+    [Fact]
+    public void should_execute_callback_through_ICustomizeManyBuildMany()
+    {
+        string shipName = null;
+        
+        Action<Ship> saveShipName = ship => shipName = ship.Name;
+        ICustomizeManyBuildMany<Ship> factory = MagicFactory.For<Ship>()
+            .Many(2)
+            .With(x => x.Name = "Mary");
+        factory
+            .Do(saveShipName)
+            .Build()
+            .ToArray();
+
+        shipName.Should().Be("Mary");
     }
 
-    public class Person
+    
+    [Fact]
+    public void should_execute_callback_through_ICustomizeOneBuildOneWithNavigation()
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        string shipName = null;
+        
+        Action<Ship> saveShipName = ship => shipName = ship.Name;
+        ICustomizeOneBuildOneWithNavigation<Ship> factory = MagicFactory.For<Ship>()
+            .One()
+            .With(x => x.Name = "Mary");
+        factory
+            .Do(saveShipName)
+            .Build();
+
+        shipName.Should().Be("Mary");
+    }
+    
+    [Fact]
+    public void should_execute_callback_through_ICustomizeOneBuildManyWithNavigation()
+    {
+        string shipName = null;
+        
+        Action<Ship> saveShipName = ship => shipName = ship.Name;
+        ICustomizeOneBuildManyWithNavigation<Ship> factory = MagicFactory.For<Ship>()
+            .One()
+            .PlusOne()
+            .With(x => x.Name = "Mary");
+        factory
+            .Do(saveShipName)
+            .Build()
+            .ToArray();
+
+        shipName.Should().Be("Mary");
+    }
+    
+    private class Ship
+    {
+        public string Name { get; set; }
     }
 }
